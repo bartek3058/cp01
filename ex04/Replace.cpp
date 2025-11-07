fa0181676423563d595964e7e9a784eaac459f86
bartek3058 <bartek3058@gmail.com>
2025-11-07 12:14:05 +0100

    ex04

--- ex04/Replace.cpp
+++ ex04/Replace.cpp
@@ -5,13 +5,17 @@ Replace::Replace(std::string filename, std::string s1, std::string s2)
 
 bool Replace::execute()
 {
+    if (s1.empty())
+    {
+        std::cerr<<"Error: s1 is empty."<<std::endl;
+        return false;
+    }
     std::ifstream inputFile(filename);
     if (!inputFile.is_open())
     {
         std::cerr<<"Error: can't open input file."<<std::endl;
         return false;
     }
-
     std::string outputFilename = filename + ".replace";
     std::ofstream outputFile(outputFilename);
     if (!outputFile.is_open())
@@ -19,18 +23,20 @@ bool Replace::execute()
         std::cerr<<"Error: can't open output file."<<std::endl;
         return false;
     }
-
     std::string content((std::istreambuf_iterator<char>(inputFile)),
                         std::istreambuf_iterator<char>());
     
-    if (s1.empty())
-    {
-        std::cerr<<"Error: s1 is empty."<<std::endl;
-        return false;
-    }
-    size_t pos = 0;
+    std::size_t pos = 0;
     while((pos = content.find(s1,pos)) != std::string::npos)
     {
-        
+        content. erase(pos, s1.length());
+        content.insert(pos, s2);
+        pos += s2.length();
     }
+
+    outputFile << content;
+
+    inputFile.close();
+    outputFile.close();
+    return true;
 }
